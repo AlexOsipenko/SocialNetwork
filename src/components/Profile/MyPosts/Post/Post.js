83a53b291da49e6e436600461f80459cdd0daef6
Alex Osipenko <yokumora@gmail.com>
2021-01-14 23:19:16 +0300

    +cosmetic post

--- src/components/Profile/MyPosts/Post/Post.js
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -3,9 +3,12 @@ import style from './Post.module.css'
 
 const Post = (props) => {
     return (
-        <div className={style.item}><img
-            src='https://avatars.mds.yandex.net/get-zen_doc/105853/pub_5cfe815283e84200af3e43e5_5cfe839285b5e500afe2eeb1/scale_1200'/>
-            {props.message}</div>
+        <div className={style.item}>
+            <div><img
+                src='https://avatars.mds.yandex.net/get-zen_doc/105853/pub_5cfe815283e84200af3e43e5_5cfe839285b5e500afe2eeb1/scale_1200'/>
+            </div>
+            <div className={style.message}>{props.message}</div>
+        </div>
     )
 }
 
