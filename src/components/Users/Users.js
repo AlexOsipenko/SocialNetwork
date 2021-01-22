a9eb293f53720772b371346a7b7637707f511201
Alex Osipenko <yokumora@gmail.com>
2021-01-22 17:58:07 +0300

    +UsersClass

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,28 +4,27 @@ import axios from "axios";
 import userPhoto from '../../static/img/no-avatar.png'
 
 
-const Users = (props) => {
-    if (props.users.length === 0){
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
-
+            props.setUsers(response.data.items)})
     }
-
-    return (
-        <div>
-            {props.users.map((user) =><div key={user.id}>
+    render() {
+        return (
+            <div>
+                {this.props.users.map((user) =><div key={user.id}>
                 <span>
                     <div>
                         <img className={style.photo}
                              src={user.photos.small != null ? user.photos.small : userPhoto}/>
                     </div>
                     <div>
-                        {user.followed ? <button onClick={()=>{props.unfollow(user.id)}}>Unfollow</button> :
-                            <button onClick={()=>{props.follow(user.id)}}>Follow</button>}
+                        {user.followed ? <button onClick={()=>{this.props.unfollow(user.id)}}>Unfollow</button> :
+                            <button onClick={()=>{this.props.follow(user.id)}}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{user.name}</div>
                         <div>{user.status}</div>
@@ -35,10 +34,12 @@ const Users = (props) => {
                         <div>{'user.location.city'}</div>
                     </span>
                 </span>
-            </div>)}
-        </div>
-
-    )
+                </div>)}
+            </div>
+        )
+    }
 }
+
+
 
 export default Users
