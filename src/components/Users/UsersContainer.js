1c8f88a54fc108f36537e6b9681bc35ea7914b26
Alex Osipenko <yokumora@gmail.com>
2021-01-27 18:30:29 +0300

    +pagination

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 import Users from "./Users";
 
 
 let mapStateToProps = (state) => {
     return {
-        users: state.UsersPage.users
+        users: state.UsersPage.users,
+        pageSize: state.UsersPage.pageSize,
+        totalUsersCount: state.UsersPage.totalUsersCount,
+        currentPage: state.UsersPage.currentPage,
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -19,7 +22,13 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users))
-        }
+        },
+        setCurrentPage: (currentPage) => {
+            dispatch(setCurrentPageAC(currentPage))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
+        },
     }
 }
 
