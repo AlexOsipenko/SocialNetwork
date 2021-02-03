395f18c840d1999d84e11fa2cc799fd1d9d61391
Alex Osipenko <yokumora@gmail.com>
2021-02-03 23:53:18 +0300

    noavatar for profile

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,11 +1,19 @@
 import React from "react";
 import style from './ProfileInfo.module.css'
 import Preloader from "../../common/preloader/preloader";
+import noavatar from '../../../static/img/no-avatar.png'
 
 const ProfileInfo = (props) => {
     if (props.profile == null){
         return(
         <Preloader/>)
+    }
+    let avatar
+    if (props.profile.photos.large != null){
+        avatar = props.profile.photos.large
+    }
+    else {
+        avatar = noavatar
     }
     return (
         <div>
@@ -13,7 +21,9 @@ const ProfileInfo = (props) => {
                 <img className={style.oblozhka} src='https://oren-m7.ru/images/400/DSC100498782.jpg'/>
             </div>
             <div>
-                <img src={props.profile.photos.large}/>
+                <img src={
+                     avatar
+                }/>
             </div>
             <div>{props.profile.aboutMe}</div>
         </div>
